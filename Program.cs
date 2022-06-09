d6cc00468adc9e4b4acdf6e2da1d42ae14249aeb
Sergey <sergeistarshark@mail.ru>
2022-06-09 08:14:12 +0300

    add token weather

--- Program.cs
+++ Program.cs
@@ -2,7 +2,8 @@
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-string url = "http://api.openweathermap.org/data/2.5/forecast?zip=171098,ru&appid=6f9b33ecd53f0c1be1b22830785323c9&cnt=40&units=metric&lang=ru";
+string token_weater = File.ReadAllText("C://token_weather.txt");
+string url = $"http://api.openweathermap.org/data/2.5/forecast?zip=171098,ru&appid={token_weater}&cnt=40&units=metric&lang=ru";
 
 HttpClient hc = new HttpClient();
 
